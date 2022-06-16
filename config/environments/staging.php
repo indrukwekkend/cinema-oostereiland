cf7eb26578e5f6f8b1d786f70046f46b29151fe2
hphioolen <hioolen@mac.com>
2022-06-16 09:34:00 +0200

    error reporting on

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -14,4 +14,14 @@ use Roots\WPConfig\Config;
  * Example: `Config::define('DISALLOW_FILE_MODS', false);`
  */
 
+Config::define('SAVEQUERIES', true);
+Config::define('WP_DEBUG', true);
+Config::define('WP_DEBUG_DISPLAY', true);
+Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?? true);
+Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
+Config::define('SCRIPT_DEBUG', true);
+Config::define('DISALLOW_INDEXING', true);
+
+ini_set('display_errors', '1');
+
 Config::define('DISALLOW_INDEXING', true);
