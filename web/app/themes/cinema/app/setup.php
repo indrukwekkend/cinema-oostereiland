84528c99228570a2091eebe944a8cd262e903f09
hphioolen <hioolen@mac.com>
2022-10-08 20:55:47 +0200

    version nr.

--- web/app/themes/cinema/app/setup.php
+++ web/app/themes/cinema/app/setup.php
@@ -11,8 +11,14 @@ use Roots\Sage\Template\BladeProvider;
  * Theme assets
  */
 add_action('wp_enqueue_scripts', function () {
-    wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, null);
-    wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], null, true);
+
+    $version = '1.1.2';
+    if(defined('WP_DEBUG') && WP_DEBUG === true){
+        $version = time();
+    }
+
+    wp_enqueue_style('sage/main.css', asset_path('styles/main.css'), false, $version);
+    wp_enqueue_script('sage/main.js', asset_path('scripts/main.js'), ['jquery'], $version, true);
 
     if (is_single() && comments_open() && get_option('thread_comments')) {
         wp_enqueue_script('comment-reply');
