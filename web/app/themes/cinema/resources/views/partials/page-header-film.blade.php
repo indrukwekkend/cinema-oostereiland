b07f629b36f8b8eb37cfeb017df68b3446529326
hphioolen <hioolen@mac.com>
2022-10-10 15:22:20 +0200

    correction play button Youtube

--- web/app/themes/cinema/resources/views/partials/page-header-film.blade.php
+++ web/app/themes/cinema/resources/views/partials/page-header-film.blade.php
@@ -40,21 +40,24 @@
 <header class="page-header-film alignfull contrast" style="background-image: url(<?=$backgroundImage?>); ">
 
 <div class="film_media">
-  <button type="button" class="kill_btn youtube_btn" id="close-youtube">
-    <i class="fa fa-times-circle fa-4x" aria-hidden="true"></i>
-  </button>
   
+    <?php if ($Youtube != '') : ?>
+      <button type="button" class="kill_btn youtube_btn" id="close-youtube">
+        <i class="fa fa-times-circle fa-2x" aria-hidden="true"></i>
+      </button>
+      <div class="fitVids-wrapper">
+          <iframe class="position-absolute" id="video" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" src="https://www.youtube.com/embed/<?= $Youtube; ?>?enablejsapi=1&amp;controls=1&amp;showinfo=0&amp;rel=0&amp;modestbranding=1" frameborder="0" allowfullscreen="" data-gtm-yt-inspected-11196688_19="true"></iframe>
+      </div>
+    <?php endif; ?>
+  </div>
+
   <?php if ($Youtube != '') : ?>
-    <div class="fitVids-wrapper">
-        <iframe class="position-absolute" id="video" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" src="https://www.youtube.com/embed/<?= $Youtube; ?>?enablejsapi=1&amp;controls=1&amp;showinfo=0&amp;rel=0&amp;modestbranding=1" frameborder="0" allowfullscreen="" data-gtm-yt-inspected-11196688_19="true"></iframe></div>
+    <div class="video-button">
+      <button type="button" class="play_btn" id="open-youtube">
+        <i class="fa fa-play-circle fa-5x" aria-hidden="true"></i>
+      </button>
     </div>
   <?php endif; ?>
-
-  <div class="video-button">
-    <button type="button" class="play_btn" id="open-youtube">
-      <i class="fa fa-play-circle fa-5x" aria-hidden="true"></i>
-    </button>
-  </div>
 
 
 
