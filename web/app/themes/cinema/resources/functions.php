3514cbf9266e7e5600ab2eceb0d2756d99b319ca
Eelco Poelstra <poelstra.eelco@gmail.com>
2025-03-03 14:01:50 +0100

    put back code in functions to hide indrukwekkend/bussiness-card in the backend

--- web/app/themes/cinema/resources/functions.php
+++ web/app/themes/cinema/resources/functions.php
@@ -109,12 +109,11 @@ Container::getInstance()
 remove_filter('get_the_excerpt', 'wp_trim_excerpt');
 
 // Verberg indrukwekkend/bussiness-card aan de achterkant
-// Verwijder deze comment
-// function custom_admin_css() {
-//     echo '<style>
-//         .wp-block-missing {
-//             display: none !important;
-//         }
-//     </style>';
-// }
-// add_action('admin_head', 'custom_admin_css');
+function custom_admin_css() {
+    echo '<style>
+        .wp-block-missing {
+            display: none !important;
+        }
+    </style>';
+}
+add_action('admin_head', 'custom_admin_css');
